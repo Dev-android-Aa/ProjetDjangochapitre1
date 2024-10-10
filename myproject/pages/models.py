8d2bbb018f392ab4aba66ccc1bb52a8b06a7d18a
AdilDev-AN <34651446+Dev-android-Aa@users.noreply.github.com>
2024-10-10 16:59:37 +0200

    create model Page and migrate it

--- myproject/pages/models.py
+++ myproject/pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    titre =models.CharField(max_length=100)
+    corp =models.TextField()
+    lingot= models.SlugField()
+    date= models.DateTimeField(auto_now_add=True)
+    
