dad7011d0c3db1ea80781bd3eef139bd7734c35d
AdilDev-AN <34651446+Dev-android-Aa@users.noreply.github.com>
2024-10-11 12:10:30 +0200

    Admin django

--- myproject/pages/urls.py
+++ myproject/pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.pages_lists),
+    path('',views.pages_lists,name="pages"),
 ]
--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
 def pages_lists(request):
-    return render(request,'pages/pages_lists.html')
+    #pages = Page.objects.all()
+    pages = Page.objects.all().order_by('date')
+    return render(request,'pages/pages_lists.html',{'pages':pages})
