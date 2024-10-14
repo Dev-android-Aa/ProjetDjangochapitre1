97e021cf3e21058f6436c340057522368071330e
AdilDev-AN <34651446+Dev-android-Aa@users.noreply.github.com>
2024-10-14 17:36:11 +0200

    Upload Image

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- myproject/pages/models.py
+++ myproject/pages/models.py
@@ -6,6 +6,7 @@ class Page(models.Model):
     corp =models.TextField()
     lingot= models.SlugField()
     date= models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(default='fallback.png',blank=True)
     
     def __str__(self) -> str:
         return self.titre
